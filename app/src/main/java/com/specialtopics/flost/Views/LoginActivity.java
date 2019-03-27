6faf370bd17cb6ea421c9bbad0d156035a6af357
Stephanie Angulo <angulostephanie21@gmail.com>
2019-03-26 20:33:53 -0700

    Added dependencies needed for firebaseauth + new login activity

---
+++ app/src/main/java/com/specialtopics/flost/Views/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.specialtopics.flost.Views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.specialtopics.flost.R;
+
+public class LoginActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
