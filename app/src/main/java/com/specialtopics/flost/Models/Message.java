acaf67b18e0011ba1b0ec842affb87960265be3f
Di <di1113@me.com>
2019-04-14 20:51:50 -0700

    chat function update [in progress]

--- app/src/main/java/com/specialtopics/flost/Models/Message.java
+++ app/src/main/java/com/specialtopics/flost/Models/Message.java
@@ -1,10 +1,64 @@
 package com.specialtopics.flost.Models;
 
+//public class Message {
+//
+//    //message_id - root
+//    //message_content
+//    //sender_id
+//    //receiver_id
+//    //timestamp
+//}
+
 public class Message {
 
-    //message_id - root
-    //message_content
-    //sender_id
-    //receiver_id
-    //timestamp
+    public static final int TYPE_MESSAGE = 0;
+    public static final int TYPE_LOG = 1;
+    public static final int TYPE_ACTION = 2;
+
+    private int mType;
+    private String mMessage;
+    private String mUsername;
+
+    private Message() {}
+
+    public int getType() {
+        return mType;
+    };
+
+    public String getMessage() {
+        return mMessage;
+    };
+
+    public String getUsername() {
+        return mUsername;
+    };
+
+
+    public static class Builder {
+        private final int mType;
+        private String mUsername;
+        private String mMessage;
+
+        public Builder(int type) {
+            mType = type;
+        }
+
+        public Builder username(String username) {
+            mUsername = username;
+            return this;
+        }
+
+        public Builder message(String message) {
+            mMessage = message;
+            return this;
+        }
+
+        public Message build() {
+            Message message = new Message();
+            message.mType = mType;
+            message.mUsername = mUsername;
+            message.mMessage = mMessage;
+            return message;
+        }
+    }
 }
