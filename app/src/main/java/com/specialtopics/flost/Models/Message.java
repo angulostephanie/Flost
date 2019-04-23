340cedbb62c2f530fd7f5d2af9c33367f52e3b4b
Di <di1113@me.com>
2019-04-22 17:25:24 -0700

    chat adapter in progress

--- app/src/main/java/com/specialtopics/flost/Models/Message.java
+++ app/src/main/java/com/specialtopics/flost/Models/Message.java
@@ -1,64 +1,72 @@
 package com.specialtopics.flost.Models;
 
-//public class Message {
-//
-//    //message_id - root
-//    //message_content
-//    //sender_id
-//    //receiver_id
-//    //timestamp
-//}
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Message {
 
-    public static final int TYPE_MESSAGE = 0;
-    public static final int TYPE_LOG = 1;
-    public static final int TYPE_ACTION = 2;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    String mMessage;
+    User mSender;
+    long createdAt;
 
-    private int mType;
-    private String mMessage;
-    private String mUsername;
+//    public static final int TYPE_MESSAGE = 0;
+//    public static final int TYPE_LOG = 1;
+//    public static final int TYPE_ACTION = 2;
+//
+//    private int mType;
+//    private String mMessage;
+//    private String mUsername;
+//
+    private Message() {
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+    }
 
-    private Message() {}
-
-    public int getType() {
-        return mType;
-    };
+//    public int getType() {
+//        return mType;
+//    };
 
     public String getMessage() {
         return mMessage;
     };
 
-    public String getUsername() {
-        return mUsername;
+    public String getSender() {
+        return mSender.getUserID();
     };
 
+    public long getCreatedAt() {
 
-    public static class Builder {
-        private final int mType;
-        private String mUsername;
-        private String mMessage;
-
-        public Builder(int type) {
-            mType = type;
-        }
-
-        public Builder username(String username) {
-            mUsername = username;
-            return this;
-        }
-
-        public Builder message(String message) {
-            mMessage = message;
-            return this;
-        }
-
-        public Message build() {
-            Message message = new Message();
-            message.mType = mType;
-            message.mUsername = mUsername;
-            message.mMessage = mMessage;
-            return message;
-        }
     }
+
+
+//    public static class Builder {
+//        private final int mType;
+//        private String mUsername;
+//        private String mMessage;
+//
+//        public Builder(int type) {
+//            mType = type;
+//        }
+//
+//        public Builder username(String username) {
+//            mUsername = username;
+//            return this;
+//        }
+//
+//        public Builder message(String message) {
+//            mMessage = message;
+//            return this;
+//        }
+//
+//        public Message build() {
+//            Message message = new Message();
+//            message.mType = mType;
+//            message.mUsername = mUsername;
+//            message.mMessage = mMessage;
+//            return message;
+//        }
+//    }
 }
