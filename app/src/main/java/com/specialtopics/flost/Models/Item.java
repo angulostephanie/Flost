6d9155b0b9529fc2c40da874d35327d6a7d1b4a0
Stephanie Angulo <angulostephanie21@gmail.com>
2019-05-02 22:42:45 -0700

    Updated rest client and item model

--- app/src/main/java/com/specialtopics/flost/Models/Item.java
+++ app/src/main/java/com/specialtopics/flost/Models/Item.java
@@ -1,79 +1,82 @@
 package com.specialtopics.flost.Models;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 public class Item {
 
-    private String itemID;
+    private int itemID;
     private String name;
     private String desc;
     private String type;
     private String location;
-    private String userID;
-    private String timeSteamp;
-    private boolean compensation;
+    private String email;
+    private String timeStamp;
+    private double compensation;
 
-    public Item(){};
+    public Item(){}
 
-    public Item(String itemID, String name, String desc, String type, String location, String userID, boolean compensation){
+    // when adding an item, timestamp and item id are generated for you
+    // timestamps are generated on the server
+    public Item(String email, String name, String desc, String type,
+                String location, double compensation) {
+        this.itemID = this.hashCode(); // based on the objects address in mem
+        this.name = name;
+        this.desc = desc;
+        this.type = type;
+        this.location = location;
+        this.email = email;
+        this.compensation = compensation;
+    }
+
+    // when fetching the time, you would provide the timestamp/item id given from the db
+    public Item(int itemID, String email, String name, String desc, String type,
+                String location, String timestamp, double compensation) {
         this.itemID = itemID;
         this.name = name;
         this.desc = desc;
         this.type = type;
         this.location = location;
-        this.userID = userID;
-        this.timeSteamp = createTimestamp();
+        this.email = email;
+        this.timeStamp = timestamp;
         this.compensation = compensation;
     }
 
-    public Item(String name, String desc, boolean compensation){
+    public Item(String name, String desc, double compensation) {
         this.name = name;
         this.desc = desc;
         this.compensation = compensation;
     }
 
-    public void setItemID(String itemID) { this.itemID = itemID; }
     public void setName(String name) { this.name = name; }
     public void setDesc(String desc) { this.desc = desc; }
     public void setType(String type) { this.type = type; }
     public void setLocation(String location) { this.location = location; }
-    public void setUserID(String userID) { this.userID = userID; }
-    public void setTimeSteamp(String timeSteamp) { this.timeSteamp = timeSteamp; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTimeStamp(String timeStamp) { this.timeStamp = timeStamp; }
+    public void setReward(double compensation) { this.compensation = compensation; }
 
-
-    public String getItemID() { return itemID; }
+    public int getItemID() { return itemID; }
     public String getName() { return name; }
     public String getDesc() { return desc; }
     public String getType() { return type; }
-    public String getLocation() { return location; }public String getUserID() { return userID; }
-    public String getTimeSteamp() { return timeSteamp; }
-
-    //item_id - root
-    //item_name
-    //item_desc
-    //item_type
-    //item_reward
-    //item_location
-    //user_id
-    //timestamp
-
-    public String createTimestamp() {
-        Calendar calendar = Calendar.getInstance();
-        Date now = calendar.getTime();
-        String timestamp = now.toString();
-        return timestamp;
-    }
+    public String getLocation() { return location; }
+    public String getEmail() { return email; }
+    public String getTimeStamp() { return timeStamp; }
+    public double getReward() { return compensation; }
 
     public static List<Item> getTemporaryData(){
         List<Item> list = new ArrayList<>();
 
-        list.add(new Item("Red Hydroflask", "Please find it!", true));
-        list.add(new Item("Pencil Case", "", false));
-        list.add(new Item("Yellow Hydroflask", "", true));
+        list.add(new Item("Red Hydroflask", "Please find it!", 20.0));
+        list.add(new Item("Pencil Case", "", 0));
+        list.add(new Item("Yellow Hydroflask", "", 15.0));
 
         return list;
+    }
+
+    public String toString() {
+        return itemID + ", " + email + ", " + name + ", " + desc + ", " + type + ", " + location
+                + ", " + timeStamp +  ", "  + compensation;
     }
 }
